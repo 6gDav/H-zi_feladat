f6fc4b9066c399d75facbe4bf2c46a2aa5cfd9a5
6gDav <118929714+6gDav@users.noreply.github.com>
2023-04-12 15:57:00 +0200

    html létrehozva és megcsinálva

---
+++ script.js
@@ -0,0 +1,14 @@
+function kiirAdatok() {
+  let data1 = document.getElementById("data1").value;
+  let data2 = document.getElementById("data2").value;
+  let data3 = document.getElementById("data3").value;
+  let data4 = document.getElementById("data4").value;
+  let data5 = document.getElementById("data5").value;
+  let data6 = document.getElementById("data6").value;
+  let data7 = document.getElementById("data7").value;
+  let data8 = document.getElementById("data8").value;
+  let data9 = document.getElementById("data9").value;
+  let data10 = document.getElementById("data10").value;
+
+  console.log("Az adatok: " + data1 + ", " + data2 + ", " + data3 + ", " + data4 + ", " + data5 + ", " + data6 + ", " + data7 + ", " + data8 + ", " + data9 + ", " + data10);
+}
